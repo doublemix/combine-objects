215f0fda234340b481a2bba3823bdacdba4d7925
Mitchel Myers <doublemx2@gmail.com>
2019-08-27 16:02:43 -0400

    Updated linting to use AirBnB style.
    - Updated relevant files.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+/* eslint-disable max-classes-per-file */
 
 import isPlainObject from 'is-plain-object';
 
@@ -6,49 +6,51 @@ import { withScalarsDeprecationWarning, opaqueFunctionDeprecationWarning } from 
 
 const EMPTY = {};
 
-function isFunction (value) {
-    return typeof value === 'function';
+function isFunction(value) {
+  return typeof value === 'function';
 }
 function isObject(value) {
-    return (typeof value === 'object') && value !== null || isFunction(value);
+  return ((typeof value === 'object') && value !== null) || isFunction(value);
 }
 
 class Replace {
-    constructor (value) {
-        this.value = value;
-    }
+  constructor(value) {
+    this.value = value;
+  }
 }
 
 class Remove {}
 class Ignore {}
 
 const symbols = {
-    opaque: '@@combineObjects/opaque',
-    remove: new Remove(),
-    ignore: new Ignore(),
-    scalars: '@@combineObjects/scalars',
+  opaque: '@@combineObjects/opaque',
+  remove: new Remove(),
+  ignore: new Ignore(),
+  scalars: '@@combineObjects/scalars',
 };
 
-function setSymbolLikeProperty (obj, property, value) {
-    Object.defineProperty(obj, property, {
-        value,
-        writable: true,
-        enumerable: false,
-        configurable: true,
-    });
+function setSymbolLikeProperty(obj, property, value) {
+  Object.defineProperty(obj, property, {
+    value,
+    writable: true,
+    enumerable: false,
+    configurable: true,
+  });
 }
 
 const replace = (obj) => new Replace(obj);
 const remove = () => symbols.remove;
 const ignore = () => symbols.ignore;
-const withScalars = (obj, scalars) => { setSymbolLikeProperty(obj, symbols.scalars, scalars); return obj; };
+const withScalars = (obj, scalars) => {
+  setSymbolLikeProperty(obj, symbols.scalars, scalars); return obj;
+};
 const opaque = (obj) => {
-    if (!isPlainObject(obj)) {
-        opaqueFunctionDeprecationWarning();
-        return replace(obj);
-    }
-    setSymbolLikeProperty(obj, symbols.opaque, true);
-    return obj;
+  if (!isPlainObject(obj)) {
+    opaqueFunctionDeprecationWarning();
+    return replace(obj);
+  }
+  setSymbolLikeProperty(obj, symbols.opaque, true);
+  return obj;
 };
 
 const shouldRemove = (obj, prop) => obj[prop] === symbols.remove;
@@ -56,83 +58,86 @@ const shouldRemove = (obj, prop) => obj[prop] === symbols.remove;
 const isReplace = (obj) => obj instanceof Replace;
 const isIgnore = (obj) => obj instanceof Ignore;
 const isOpaque = (obj) => isObject(obj) && !!obj[symbols.opaque];
-const hasScalars = (obj) => isObject(obj) && obj.hasOwnProperty(symbols.scalars);
+const hasScalars = (obj) => (
+  isObject(obj) && Object.prototype.hasOwnProperty.call(obj, symbols.scalars)
+);
 const getScalars = (obj) => obj[symbols.scalars];
 const isScalarProp = (obj, prop) => hasScalars(obj) && getScalars(obj).indexOf(prop) !== -1;
 
-function removeIfNecessary (obj, prop) {
-    if (shouldRemove(obj, prop)) {
-        delete obj[prop];
-    }
-    return obj; // fluency
+function removeIfNecessary(obj, prop) {
+  if (shouldRemove(obj, prop)) {
+    // eslint-disable-next-line no-param-reassign
+    delete obj[prop];
+  }
+  return obj; // fluency
 }
 
-function combineObjects (source, update) {
-    const result = {};
-    if (hasScalars(source)) {
-        withScalarsDeprecationWarning();
-        withScalars(result, getScalars(source));
+function combineObjects(source, update) {
+  const result = {};
+  if (hasScalars(source)) {
+    withScalarsDeprecationWarning();
+    withScalars(result, getScalars(source));
+  }
+  [
+    ...Object.keys(source),
+    ...Object.keys(update),
+  ].forEach((prop) => {
+    if (Object.prototype.hasOwnProperty.call(result, prop)) {
+      return; // already processed
     }
-    [
-        ...Object.keys(source),
-        ...Object.keys(update),
-    ].forEach((prop) => {
-        if (result.hasOwnProperty(prop)) {
-            return; // already processed
-        }
-        if (update.hasOwnProperty(prop)) {
-            // mutually recursive functions
-            // eslint-disable-next-line no-use-before-define
-            result[prop] = internalCombine(source[prop], update[prop], prop, isScalarProp(source, prop));
-            removeIfNecessary(result, prop);
-        } else {
-            result[prop] = source[prop];
-        }
-    });
+    if (Object.prototype.hasOwnProperty.call(update, prop)) {
+      // mutually recursive functions
+      // eslint-disable-next-line no-use-before-define
+      result[prop] = internalCombine(source[prop], update[prop], prop, isScalarProp(source, prop));
+      removeIfNecessary(result, prop);
+    } else {
+      result[prop] = source[prop];
+    }
+  });
 
-    return result;
-}
-
-function shouldReplace (source, update) {
-    return (
-        !isPlainObject(source) ||
-        !isPlainObject(update) ||
-        isOpaque(source)
-    );
+  return result;
 }
 
-function internalCombine (source, update, key = undefined, isScalarField = false) {
-    if (isFunction(update)) {
-        return internalCombine(source, update(source, key), key, isScalarField);
-    }
-    if (isOpaque(update)) {
-        return update;
-    }
-    if (isReplace(update)) {
-        return update.value;
-    }
-    if (isIgnore(update)) {
-        return source;
-    }
-    if (shouldReplace(source, update)) {
-        if (isPlainObject(update)) {
-            return combineObjects(EMPTY, update); // allows nested function transforms to happen
-        }
-        return update;
-    }
-    if (isScalarField) {
-        return update;
-    }
-    return combineObjects(source, update);
+function shouldReplace(source, update) {
+  return (
+    !isPlainObject(source)
+        || !isPlainObject(update)
+        || isOpaque(source)
+  );
 }
-function internalCombine2 (source, update) {
-    const result = internalCombine(source, update);
-    return result === symbols.remove ? undefined : result;
+
+function internalCombine(source, update, key = undefined, isScalarField = false) {
+  if (isFunction(update)) {
+    return internalCombine(source, update(source, key), key, isScalarField);
+  }
+  if (isOpaque(update)) {
+    return update;
+  }
+  if (isReplace(update)) {
+    return update.value;
+  }
+  if (isIgnore(update)) {
+    return source;
+  }
+  if (shouldReplace(source, update)) {
+    if (isPlainObject(update)) {
+      return combineObjects(EMPTY, update); // allows nested function transforms to happen
+    }
+    return update;
+  }
+  if (isScalarField) {
+    return update;
+  }
+  return combineObjects(source, update);
 }
-function combine (source, ...updates) {
-    return updates.reduce((src, update) => (
-        internalCombine2(src, update)
-    ), source);
+function internalCombine2(source, update) {
+  const result = internalCombine(source, update);
+  return result === symbols.remove ? undefined : result;
+}
+function combine(source, ...updates) {
+  return updates.reduce((src, update) => (
+    internalCombine2(src, update)
+  ), source);
 }
 
 combine.replace = replace;
--- src/warnings.js
+++ src/warnings.js
@@ -1,13 +1,17 @@
-export function deprecationWarning (message) {
-    let displayed = false;
-    return function _deprecationWarning () {
-        if (!displayed) {
-            // eslint-disable-next-line no-console
-            console.warn(message);
-            displayed = true;
-        }
-    };
+export function deprecationWarning(message) {
+  let displayed = false;
+  return function _deprecationWarning() {
+    if (!displayed) {
+      // eslint-disable-next-line no-console
+      console.warn(message);
+      displayed = true;
+    }
+  };
 }
 
-export const withScalarsDeprecationWarning = deprecationWarning('`withScalars` is deprecated and set to be removed in v1.0.0');
-export const opaqueFunctionDeprecationWarning = deprecationWarning('`opaque` should only be used with plain objects. Try using `replace` instead');
+export const withScalarsDeprecationWarning = deprecationWarning(
+  '`withScalars` is deprecated and set to be removed in v1.0.0',
+);
+export const opaqueFunctionDeprecationWarning = deprecationWarning(
+  '`opaque` should only be used with plain objects. Try using `replace` instead',
+);
